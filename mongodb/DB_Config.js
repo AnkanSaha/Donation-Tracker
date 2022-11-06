c508aedb9b1ac7ae81099ee312bc2391ea214cd1
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-11-07 03:34:58 +0530

    Update DB_Config.js

--- mongodb/DB_Config.js
+++ mongodb/DB_Config.js
@@ -1,5 +1,2 @@
 const DB_Url = `mongodb://localhost:27017/Donation`; // MongoDB Connection URL
 module.exports.url = DB_Url;
-
-
-//mongodb+srv://Ankan:Ankan1567@resultshowdatabse.q1ops.mongodb.net/Donation?retryWrites=true&w=majority
